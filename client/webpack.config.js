0d256023136a89b06bc3feafc25ef6a4f691c073
CF <cfreitas12345@gmail.com>
2023-05-04 00:03:33 -0700

    Initial commit

---
+++ client/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+// // ES6+
+import WebpackPwaManifest from 'webpack-pwa-manifest'
+
+// ES5
+var WebpackPwaManifest = require('webpack-pwa-manifest')
+ 
+plugins: [
+  new WebpackPwaManifest({
+    name: 'My Progressive Web App',
+    short_name: 'MyPWA',
+    description: 'My awesome Progressive Web App!',
+    background_color: '#ffffff',
+    crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+    icons: [
+      {
+        src: path.resolve('src/assets/icon.png'),
+        sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+      },
+      {
+        src: path.resolve('src/assets/large-icon.png'),
+        size: '1024x1024' // you can also use the specifications pattern
+      },
+      {
+        src: path.resolve('src/assets/maskable-icon.png'),
+        size: '1024x1024',
+        purpose: 'maskable'
+      }
+    ]
+  })
+]
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      cards: './src/js/cards.js'
+    },
+
+    // TODO: Add the correct output
+    output: {
+      
+    },
+
+    // TODO: Add the correct plugins
+    plugins: [
+     
+    ],
+
+    // TODO: Add the correct modules
+    module: {
+
+    }
+  };
+};
