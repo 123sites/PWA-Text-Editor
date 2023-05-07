1429a1f66ceaa497b91d2e60f50d3de626d5c019
CF <cfreitas12345@gmail.com>
2023-05-06 22:59:16 -0700

    Changed the code in index & database, added README

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,34 +1,59 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-
+// Adds an event handler to the `beforeinstallprompt` event
+window.addEventListener("beforeinstallprompt", (event) => {
   // Store the triggered events
   window.deferredPrompt = event;
-
-  // Remove the hidden class from the button.
-  butInstall.classList.toggle('hidden', false);
+  // Removes the hidden class from the button.
+  butInstall.classList.toggle("hidden", false);
 });
 
-butInstall.addEventListener('click', async () => {
+// Implements a click event handler on the `butInstall` element
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
 
-const promptEvent = window.deferredPrompt;
-
-if (!promptEvent) {
- return;
-}
-
-// Show prompt
-promptEvent.prompt();
-
-// Reset the deferred prompt variable, it can only be used once.
-window.deferredPrompt = null;
-
-butInstall.classList.toggle('hidden', true);
+  // Show prompt
+  promptEvent.prompt();
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  butInstall.classList.toggle("hidden", true);
 });
 
-window.addEventListener('appinstalled', (event) => {
-// Clear prompt
-window.deferredPrompt = null;
-}); 
+// Adds a handler for the `appinstalled` event
+window.addEventListener("appinstalled", (event) => {
+
+    // const jate = form.elements['jate'].value;
+    // // Post form data to IndexedDB
+    // postDb(jate);
+    // form.reset();
+    // fetchList();
+  // Clear prompt
+  window.deferredPrompt = null;
+});
+
+//   document.getElementById("click", appInstalled (){ alert("Hello World!");
+// });
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/appinstalled_event
+//   onappinstalled = (event) => {};
+//   console.log{"Thank you for installing our App!"}
+// }):
+
+
+  // // Line 29-35, as-in lesson 26.
+  //   // handle the form data
+  //   event.preventDefault();
+//     document.getElementById("")
+
+//     const jate = form.elements['jate'].value;
+//     // Post form data to IndexedDB
+//     postDb(jate);
+//     form.reset();
+//     fetchList();
+//   // Clear prompt
+//   window.deferredPrompt = null;
+// });
--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# PWA-Text-Editor
+# ⭐ PWA-Text-Editor ⭐
+
+![alt text](https://img.shields.io/badge/License-MIT-blue.svg)
+
+![badmath](https://img.shields.io/badge/JavaScript-35%25-purple)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![badmath](https://img.shields.io/badge/CSS-41%25-purple)
+
+## Description ✏️
+
+Jate is a text editor web application that can work without internet connection.
+You can use it through the Heroku link on "Installation & Heroku Deployment" below.
+
+## User Story 📖
+
+This is for creating notes, whether you are on the internet or not and is reliable to retrieve them for 
+later use.
+
+
+## Table of Contents 📖
+
+- [Installation & Heroku Demonstration](#installation&demo)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Credits](#credits)
+- [License](#license)
+- [Features](#features)
+
+## Installation & Heroku Demonstration 🔑
+
+1.  Clone this repo.
+2.  Do an npm i.
+3.  This can be run on Heroku.
+
+## Usage &#128161;
+
+[Repository link](https://github.com/123sites/PWA-Text-Editor)
+
+## Tests 🎯
+
+&nbsp; None
+
+## Credits 👈
+
+For this Challenge, I utilized code from the 19-PWA Bootcamp folder and 
+I reached-out to get help from a Bootcamp tutor, when ever needed.
+
+## License 📝
+
+The MIT license is in the GitHub repository. Click to link to view it:
+[MIT License](https://github.com/123sites/PWA-Text-Editor/blob/main/LICENSE)
+
+## Features ✍
+
+
